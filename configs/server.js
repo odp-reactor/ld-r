525ca5f782770b4bf443194faf598a11a4036af7
linux <christian.colonna@studio.unibo.it>
2020-07-29 16:11:03 +0200

    restored configurations and removed from .gitignore

---
+++ configs/server.js
@@ -0,0 +1,25 @@
+//important: first value in the array is considered as default value for the property
+//this file is visible to the server-side
+export default {
+    serverPort: [4000],
+    sparqlEndpoint: {
+        'generic': {
+            host: 'localhost', port: 8890, path: '/sparql', endpointType: 'virtuoso'
+        },
+	'http://virtuoso.local/configurations': {
+	    host: 'localhost', port: 8890, path: '/sparql', endpointType: 'virtuoso'
+	},
+    },
+    dbpediaLookupService: [
+        { host: 'lookup.dbpedia.org' }
+    ],
+    dbpediaSpotlightService: [
+        { host: 'api.dbpedia-spotlight.org', port: 80, path: '/en/annotate' }
+    ],
+    //it is used only if you enabled recaptcha feature for user authentication
+    //get keys from https://www.google.com/recaptcha
+    googleRecaptchaService: {
+        siteKey: ['put your google recaptcha site key here...'],
+        secretKey: ['put your google recaptcha secret key here...']
+    }
+};
