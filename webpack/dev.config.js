39fd615e960e08313f3d7eccfc2f7a31f96b96b5
col1n <col1n@boston.local>
2020-09-03 15:02:28 +0200

    added webpack loader for images

--- webpack/dev.config.js
+++ webpack/dev.config.js
@@ -32,6 +32,10 @@ let webpackConfig = {
                 exclude: /node_modules/,
                 loader: 'babel-loader'
             },
+	    {
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                loader: 'url-loader?limit=100000'
+	    },
             { test: /\.css$/,
                 use: [
                     {
