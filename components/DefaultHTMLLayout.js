8ced0c95fb0776f8889b57ad73f568dca4ed8ef4
col1n <col1n@boston.local>
2021-01-25 15:59:35 +0100

    ciao

--- components/DefaultHTMLLayout.js
+++ components/DefaultHTMLLayout.js
@@ -121,13 +121,15 @@ class DefaultHTMLLayout extends React.Component {
                     ></script>
                     {/* All external vendors bundle*/
                         this.props.addAssets ? (
-                            <script src={'/public/js/vendor.bundle.js'}></script>
+                            <script
+                                src={`${PUBLIC_URL}/public/js/vendor.bundle.js`}
+                            ></script>
                         ) : (
                             ''
                         )}
                     {/* Main app bundle */}
                     <script
-                        src={'/public/js/' + this.props.clientFile}
+                        src={`${PUBLIC_URL}/public/js/` + this.props.clientFile}
                     ></script>
                     {googleAnalyticsID && (
                         <script
