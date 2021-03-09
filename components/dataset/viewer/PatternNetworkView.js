496d4c1cf4712acdae320486dbc7042a3f5daf40
col1n <col1n@boston.local>
2021-03-09 11:45:15 +0100

    modify size and color for mobile nodes

--- components/dataset/viewer/PatternNetworkView.js
+++ components/dataset/viewer/PatternNetworkView.js
@@ -107,9 +107,12 @@ export default class PatternNetworkView extends React.Component {
                         type: 'Pattern',
                         occurences: p.occurences,
                         nodeSize: 15,
+                        nodeMobileSize: 15,
                         nodeLabelSize: 20,
                         nodeColor: 'purple',
                         nodeBorderColor: 'purple',
+                        nodeMobileColor: 'thistle',
+                        nodeBorderMobileColor: 'purple',
                         nodeType: 'graphin-circle',
                         mobileNodeType: 'circle',
                         tooltipInfo: dataInfoMap[p.pattern],
@@ -170,12 +173,15 @@ export default class PatternNetworkView extends React.Component {
                         type: 'Class',
                         centralityScore: c.pd,
                         nodeSize: 60,
+                        nodeMobileSize: 60 * 5,
                         nodeLabelSize: 20,
                         nodeType: 'diamond',
-                        mobileNodeType: 'diamond',
+                        mobileNodeType: 'triangle',
                         nodeLabelPosition: 'bottom',
                         nodeColor: 'red',
                         nodeBorderColor: 'red',
+                        nodeMobileColor: '#ffcccb',
+                        nodeBorderMobileColor: 'red',
                         scaledCentralityScore: scaleInto01(
                             c.pd,
                             minCentralityScore,
@@ -234,21 +240,25 @@ export default class PatternNetworkView extends React.Component {
             // update pattern size
             kg.forEachPattern((resourceURI, attributes) => {
                 if (kg.getResourceProperty(resourceURI, 'occurences')) {
+                    const scaledSize = Math.round(
+                        scaleData(
+                            kg.getResourceProperty(resourceURI, 'occurences'),
+                            0,
+                            350,
+                            20,
+                            80
+                        )
+                    );
+
                     kg.updateResourceProperty(
                         resourceURI,
                         'nodeSize',
-                        Math.round(
-                            scaleData(
-                                kg.getResourceProperty(
-                                    resourceURI,
-                                    'occurences'
-                                ),
-                                0,
-                                350,
-                                20,
-                                80
-                            )
-                        )
+                        scaledSize
+                    );
+                    kg.updateResourceProperty(
+                        resourceURI,
+                        'nodeMobileSize',
+                        scaledSize * 10
                     );
                 }
             });
