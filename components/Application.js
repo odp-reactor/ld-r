bd9a89f22de09536ccab83746126cd74060b3ff0
col1n <col1n@boston.local>
2021-03-30 16:26:50 +0200

    removed matomo tracking

--- components/Application.js
+++ components/Application.js
@@ -10,17 +10,17 @@ import ApplicationStore from '../stores/ApplicationStore';
 import { connectToStores, provideContext } from 'fluxible-addons-react';
 import { handleHistory } from 'fluxible-router';
 import ErrorHandler from './ErrorHandler';
-import MatomoTracker from '@datapunt/matomo-tracker-js';
+//import MatomoTracker from '@datapunt/matomo-tracker-js';
 
 import regeneratorRuntime from 'regenerator-runtime';
-import { hotjar } from 'react-hotjar';
+//import { hotjar } from 'react-hotjar';
 
 class Application extends React.Component {
     constructor(props, context) {
         super(props, context);
-        this.state = {
-            matomoTracker: null
-        };
+        // this.state = {
+        //     matomoTracker: null
+        // };
     }
     componentDidUpdate(prevProps) {
         let newProps = this.props;
@@ -32,33 +32,33 @@ class Application extends React.Component {
         }
         document.title = newProps.ApplicationStore.pageTitle;
     }
-    componentDidMount() {
-        // hotjar.initialize(2294500, 6);
-        if (!this.state.matomoTracker) {
-            const tracker = new MatomoTracker({
-                urlBase: 'https://cnr.matomo.cloud/',
-                siteId: 6,
-                disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
-                heartBeat: {
-                    // optional, enabled by default
-                    active: true, // optional, default value: true
-                    seconds: 10 // optional, default value: `15
-                },
-                linkTracking: true, // optional, default value: true
-                configurations: {
-                    // optional, default value: {}
-                    // any valid matomo configuration, all below are optional
-                }
-            });
-            this.setState({ matomoTracker: tracker });
-        }
-    }
+    // componentDidMount() {
+    //     // hotjar.initialize(2294500, 6);
+    //     if (!this.state.matomoTracker) {
+    //         const tracker = new MatomoTracker({
+    //             urlBase: 'https://cnr.matomo.cloud/',
+    //             siteId: 6,
+    //             disabled: false, // optional, false by default. Makes all tracking calls no-ops if set to true.
+    //             heartBeat: {
+    //                 // optional, enabled by default
+    //                 active: true, // optional, default value: true
+    //                 seconds: 10 // optional, default value: `15
+    //             },
+    //             linkTracking: true, // optional, default value: true
+    //             configurations: {
+    //                 // optional, default value: {}
+    //                 // any valid matomo configuration, all below are optional
+    //             }
+    //         });
+    //         this.setState({ matomoTracker: tracker });
+    //     }
+    // }
     render() {
         var Handler = this.props.currentRoute.handler;
-        if (this.state.matomoTracker) {
-            console.log('Tracking matomo');
-            this.state.matomoTracker.trackPageView();
-        }
+        // if (this.state.matomoTracker) {
+        //     console.log('Tracking matomo');
+        //     this.state.matomoTracker.trackPageView();
+        // }
         //render content
         return (
             <ErrorHandler>
