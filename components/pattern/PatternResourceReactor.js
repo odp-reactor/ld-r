e5a7baed42939bffbbee1ec3e6858c6d3c2928d4
colin <colin@ubuntu.com>
2021-07-26 04:29:02 -0400

    add status check to control fetching and rendering of PatternReactor and PatternResourceReactor

--- components/pattern/PatternResourceReactor.js
+++ components/pattern/PatternResourceReactor.js
@@ -6,6 +6,7 @@ import DbClient from '../../services/base/DbClient';
 import MosaicVisualFrame from './MosaicVisualFrame';
 
 import {CustomLoader} from '../CustomLoader'
+import {fetchInstanceStatusEnum} from './fetchInstanceStatusEnum'
 
 const configEndpoint = process.env.CONFIG_SPARQL_ENDPOINT_URI;
 const serverConfigRepo = new ServerConfigRepository(
@@ -16,28 +17,43 @@ export class PatternResourceReactor extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            patternInstances: undefined,
-            dbContext: undefined
+            patternInstances: null,
+            dbContext: null,
+            status: fetchInstanceStatusEnum.LOADING
         }
     }
 
     componentDidMount() {
         const getPatternInstancesByResource = async (datasetId, resourceURI) => {
             if (datasetId && resourceURI) {
-                const dbContext = await serverConfigRepo.getSparqlEndpointAndGraphByDatasetId(datasetId)
-                const dbClient = new DbClient(dbContext.sparqlEndpoint, dbContext.graph)
-                const patternRepo = new PatternInstanceRepository(dbClient)
-                if (dbContext && dbContext.sparqlEndpoint) {
-                    const patternInstances = await patternRepo.getPatternInstancesByResourceParticipatingInPattern(resourceURI)
-                    if (patternInstances) {
-                        this.setState({
-                            patternInstances: patternInstances,
-                            dbContext: dbContext
-                        })
+                try {
+                    const dbContext = await serverConfigRepo.getSparqlEndpointAndGraphByDatasetId(datasetId)
+                    const dbClient = new DbClient(dbContext.sparqlEndpoint, dbContext.graph)
+                    const patternRepo = new PatternInstanceRepository(dbClient)
+                    if (dbContext && dbContext.sparqlEndpoint) {
+                        const patternInstances = await patternRepo.getPatternInstancesByResourceParticipatingInPattern(resourceURI)
+                        if (patternInstances) {
+                            this.setState({
+                                patternInstances: patternInstances,
+                                dbContext: dbContext,
+                                status: fetchInstanceStatusEnum.FOUND
+                            })
+                        } else {
+                            this.setState({
+                                status: fetchInstanceStatusEnum.NOT_FOUND
+                            })
+                        }
                     }
-                }
+                } catch (err) {
+                    console.log('[!] Error ', err)
+                    this.setState({
+                        status: fetchInstanceStatusEnum.FETCHING_ERROR
+                    })   
+                } 
             } else {
-                // console.log(`[!] No datasetId or resource uri. Cannot retrieve associated sparql endpoint. Dataset Id: ${datasetId} ; Pattern Instance Uri: ${resourceURI}`)
+                this.setState({
+                    status: fetchInstanceStatusEnum.SPARQL_ENDPOINT_NOT_VALID
+                })
             }
         }
         getPatternInstancesByResource(this.props.datasetURI, this.props.resourceURI)
@@ -47,11 +63,11 @@ export class PatternResourceReactor extends React.Component {
 
         // se fanno parte di stessa istanza aggregarli e passare istanze di pattern ai visual frame
 
-        if (!this.state.patternInstances) {
+        if (this.state.status === fetchInstanceStatusEnum.LOADING) {
             // fetching => loader
             return <div style={{textAlign:'center'}}><CustomLoader /></div>
         } else {
-            if (this.state.patternInstances.length > 0) {
+            if (this.state.status === fetchInstanceStatusEnum.FOUND && this.state.patternInstances.length > 0) {
                 // fetched: length > 0 => mosaic
                 return <MosaicVisualFrame patternInstances={this.state.patternInstances} dbContext={this.state.dbContext} />
             } else {
